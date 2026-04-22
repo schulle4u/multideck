abfe5799ee4cd980252b1b53169a5a9c36bb5c45
Steffen Schultz <steffenschultz@mailbox.org>
2026-04-22 14:05:31 +0200

    Bump version

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,5 @@ MultiDeck Audio Player
 Accessible cross-platform audio player for simultaneous playback
 """
 
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 __author__ = 'Steffen Schultz'
--- src/config/defaults.py
+++ src/config/defaults.py
@@ -119,7 +119,7 @@ VALID_DECK_RANGE = [1, 128]
 
 # Application info
 APP_NAME = 'MultiDeck Audio Player'
-APP_VERSION = '0.6.0'
+APP_VERSION = '0.7.0'
 APP_AUTHOR = 'Steffen Schultz'
 APP_WEBSITE = 'https://m45.dev'
 APP_LICENSE = 'MIT License'
